4af95f5fad296c995778781019ca0c0915364e9a
dionberisha23 <94796934+dionberisha23@users.noreply.github.com>
2026-04-30 23:10:40 +0200

    feat(filters): implement passing failing and subject routes

--- src/Routes/Filters.hs
+++ src/Routes/Filters.hs
@@ -2,12 +2,36 @@
 
 module Routes.Filters (mountFiltersRoutes) where
 
-import Data.IORef
+import Analyzer (filterBySubject, getFailingStudents, getPassingStudents)
+import Control.Monad.IO.Class (liftIO)
+import Data.Aeson (object, (.=))
+import Data.IORef (IORef, readIORef)
+import Network.HTTP.Types.Status (status400)
+import Text.Read (readMaybe)
+import Types (GradeRecord, Subject)
 import Web.Scotty
-import Types (GradeRecord)
 
 mountFiltersRoutes :: IORef [GradeRecord] -> ScottyM ()
-mountFiltersRoutes _ = do
-  get "/api/passing" $ json ([] :: [GradeRecord])
-  get "/api/failing" $ json ([] :: [GradeRecord])
-  get "/api/subject/:name" $ json ([] :: [GradeRecord])
+mountFiltersRoutes recordsRef = do
+  get "/api/passing" $ do
+    records <- liftAndRead recordsRef
+    json (getPassingStudents records)
+
+  get "/api/failing" $ do
+    records <- liftAndRead recordsRef
+    json (getFailingStudents records)
+
+  get "/api/subject/:name" $ do
+    name <- param "name"
+    records <- liftAndRead recordsRef
+    case parseSubject name of
+      Nothing -> do
+        status status400
+        json $ object ["error" .= ("Invalid subject" :: String)]
+      Just subject -> json (filterBySubject subject records)
+
+liftAndRead :: IORef [GradeRecord] -> ActionM [GradeRecord]
+liftAndRead = liftIO . readIORef
+
+parseSubject :: String -> Maybe Subject
+parseSubject = readMaybe
